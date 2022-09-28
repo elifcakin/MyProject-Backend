9fe32aa74ea419d429508e4bdf258a83e066ed57
Atk <atakanarslan@msn.com>
2022-09-28 17:50:24 +0300

    28.09.22
    17.50

--- ws/src/main/java/com/hoaxify/ws/user/User.java
+++ ws/src/main/java/com/hoaxify/ws/user/User.java
@@ -2,28 +2,35 @@ package com.hoaxify.ws.user;
 
 
  
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+
 import lombok.Data;
+
 
 @Data 
 @Entity
 @Table(name="users")
+
 public class User {
+	
 	
 	@Id
 	@GeneratedValue
 	private long id; 
 	
-	@NotNull
+	
+	@NotNull(message="{hoaxify.constraints.username.NotNull.message}")
 	@Size(min =4, max= 255)
+	@UniqueUsername
 	private String username;
 	
     @NotNull
@@ -31,7 +38,7 @@ public class User {
 	private String displayName;
 	
     @NotNull
-    @Size(min=4, max=255)
+    @Size(min=5, max=255)
 	private String password;
 
 	
