ca7c068d852cb8216854ceef5d4ed7e157b59533
Atk <atakanarslan@msn.com>
2022-10-26 17:18:01 +0300

    26.10.22

--- ws/src/main/java/com/hoaxify/ws/WsApplication.java
+++ ws/src/main/java/com/hoaxify/ws/WsApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
+import com.hoaxify.ws.hoax.Hoax;
+import com.hoaxify.ws.hoax.HoaxService;
 import com.hoaxify.ws.user.User;
 import com.hoaxify.ws.user.UserService;
 
@@ -16,14 +19,20 @@ public class WsApplication {
 	}
 	
 	@Bean
-	CommandLineRunner createInitialUsers(UserService userService) {
+	@Profile("dev")
+	CommandLineRunner createInitialUsers(UserService userService, HoaxService hoaxService) {
 		return (args) -> {
-			for(int i = 1; i<=10;i++) {				
+			for(int i = 1; i<=25;i++) {				
 				User user = new User();
 				user.setUsername("user"+i);
 				user.setDisplayName("display"+i);
 				user.setPassword("P4ssword");
 				userService.save(user);
+				for(int j = 1;j<=20;j++) {
+					Hoax hoax = new Hoax();
+					hoax.setContent("hoax (" +j + ") from user ("+i+")");
+					hoaxService.save(hoax, user);
+				}
 			}
 		};
 	}
--- ws/src/main/java/com/hoaxify/ws/user/User.java
+++ ws/src/main/java/com/hoaxify/ws/user/User.java
@@ -4,7 +4,9 @@ import java.util.Collection;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
@@ -17,6 +19,7 @@ import lombok.Data;
 
 @Data
 @Entity
+@Table(name="users")
 public class User implements UserDetails{
 	
 	/**
@@ -25,7 +28,7 @@ public class User implements UserDetails{
 	private static final long serialVersionUID = -8421768845853099274L;
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	
 	@NotNull(message="{hoaxify.constraint.username.NotNull.message}")
@@ -39,8 +42,9 @@ public class User implements UserDetails{
 	
 	@NotNull
 	@Size(min = 8, max=255)
+	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message="{hoaxify.constrain.password.Pattern.message}")
 	private String password;
-	
+
 	private String image;
 
 	@Override
